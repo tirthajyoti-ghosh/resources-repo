b9597cfc603c2ed43a03c7e2bdcce5a099dc9c54
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-06-20 21:15:56 +0530

    fix unexpected (:) in comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-validates: :body, presence: true, length: {minimum: 10, maximum: 80}
+  validates :body, presence: true, length: {minimum: 10, maximum: 80}
   belongs_to :user
   belongs_to :resource
 end
