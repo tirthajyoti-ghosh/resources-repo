50149ccde456f58f61200cd5d60be164bcc4ebb6
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-06-20 18:26:45 +0530

    add name field to users table

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-    has_many :categorizations
-    has_many :resources, through: :categorizations
+	has_many :categorizations
+	has_many :resources, through: :categorizations
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,8 +1,7 @@
 class Resource < ApplicationRecord
-    belongs_to :user
-    has_many :comments
-    has_many :categorizations
-    has_many :categories, through: :categorizations
-    has_many :votes
-
+	belongs_to :user
+	has_many :comments
+	has_many :categorizations
+	has_many :categories, through: :categorizations
+	has_many :votes
 end
