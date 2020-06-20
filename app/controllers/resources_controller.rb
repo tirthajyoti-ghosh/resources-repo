03fd1acdd549133d2987c5163efadb1bc979eb93
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-06-20 18:59:01 +0530

    add rest of the resource actions. modify views

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,16 +1,59 @@
 class ResourcesController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_resource, only: [:show, :edit, :update, :destroy]
 
   def index
+    @resources = Resource.all
   end
 
   def show
   end
-
+  
   def new
     @resource = Resource.new
   end
 
   def edit
+  end
+
+  def create
+    @resource = current_user.resources.build(resource_params)
+    if @resource.save
+      flash[:success] = "Resource successfully created!"
+      redirect_to @resource
+    else
+      flash[:error] = "Something went wrong."
+      render 'new'
+    end
+  end
+
+  def update
+    if @resource.update_attributes(resource_params)
+      flash[:success] = "Resource was successfully updated!"
+      redirect_to @resource
+    else
+      flash[:error] = "Something went wrong."
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @resource.destroy
+      flash[:success] = 'Resource was successfully deleted.'
+      redirect_to root_path
+    else
+      flash[:error] = 'Something went wrong.'
+      redirect_to @resource
+    end
+  end  
+  
+  private
+  
+  def set_resource
+    @resource = Resource.find(params[:id])
+  end
+
+  def resource_params
+    params.require(:resource).permit(:link, :notes)
   end
 end
