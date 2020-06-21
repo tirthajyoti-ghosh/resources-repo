220e95decb93a917e449430dad22601fec9cbc71
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-06-21 22:22:02 +0530

    add categories controller, configure routes to take name

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'categories', to: "categories#index"
+  get 'categories/:name', to: "categories#show", as: :category
   devise_for :users
   resources :resources
   root to: 'resources#index'
