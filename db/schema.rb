944a9fb7461c5b0254ed20742da334f8befd5b74
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-07-11 22:06:21 +0530

    Add title to resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_125359) do
+ActiveRecord::Schema.define(version: 2020_07_11_162109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_06_20_125359) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
     t.index ["user_id"], name: "index_resources_on_user_id"
   end
 
