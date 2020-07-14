2b181847e2b3c04f776897dabeb89e677811828c
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-07-14 12:20:24 +0530

    Add comments controller
    - Setup routes for comments
    - Remove all Test::Unit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'categories', to: "categories#index"
   get 'categories/:name', to: "categories#show", as: :category
   devise_for :users
-  resources :resources
+  resources :resources do
+    resources :comments, except: [:show]
+  end
   root to: 'resources#index'
 end
