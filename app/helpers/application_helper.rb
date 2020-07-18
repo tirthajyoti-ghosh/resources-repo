0741d4fb919bf04a805609e34ea33c97e86cc433
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-07-18 16:42:27 +0530

    Add voting on resources functionality
    - Add helper method to take care of UI of voting buttons
    - Add fontawesome
    - Add default_scope -> { order(created_at: :desc) } in resource model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def display_vote_for(resource, user)
+    vote_count = if resource.votes_count.nil?
+                   0
+                 else
+                   resource.votes_count
+                 end
+    
+    if user.nil?
+      content_tag(:p, class: 'voted') do
+        "<i class='far fa-heart'></i> #{vote_count}".html_safe
+      end
+    else
+      vote_status = Resource.already_voted?(resource, user.id)    
+
+      if vote_status
+        content_tag(:p, class: 'voted') do
+          "<i class='fas fa-heart'></i> #{vote_count}".html_safe
+        end
+      else
+        link_to(resource_votes_path(resource), method: :post, class: 'vote') do
+          "<i class='far fa-heart'></i> #{vote_count}".html_safe
+        end
+      end
+    end
+  end
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -9,6 +9,8 @@ class Resource < ApplicationRecord
 	validates :link, presence: true
 	validates :notes, presence: true, length: {minimum: 10, maximum: 140}
 
+	default_scope -> { order(created_at: :desc) }
+
 	def category_list=(category_string)
     category_names = category_string.split(',').collect { |s| s.strip }.uniq
     new_or_found_categories = category_names.collect { |name| Category.find_or_create_by(name: name) }
@@ -19,7 +21,7 @@ class Resource < ApplicationRecord
 		categories.join(' ')
 	end	
 
-  def self.already_voted?(article, user_id)
-    Vote.exists?(user_id: user_id, article_id: article.id)
+  def self.already_voted?(resource, user_id)
+    Vote.exists?(user_id: user_id, resource_id: resource.id)
   end
 end
